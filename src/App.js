4f219d7cfaf9ce84d9074b0fc3042c183ac8a143
UvinduHewage <uvinduudakaramagedarah@gmail.com>
2025-05-11 20:40:58 +0530

    path and backgrond changes

--- src/App.js
+++ src/App.js
@@ -5,15 +5,20 @@ import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import Background from './components/Background';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
+    <div className="App bg-black">
+      <Background>
+        <div className="app-content">
+          <Home />
+          <About />
+          <Skills />
+          <Projects />
+          <Contact />
+        </div>
+      </Background>
     </div>
   );
 }
